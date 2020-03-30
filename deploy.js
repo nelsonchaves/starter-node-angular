eeef2de8945fd94c4dd1836365b375572567a400
Nelson Chaves <nelsonchavespro@gmail.com>
2020-03-29 19:49:53 -0700

    Travis CI Deployment Automation9

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.21.240.7',
+      host: '18.221.235.62',
       username: 'ubuntu',
       privateKey: 'sna-key.pem'
     })
