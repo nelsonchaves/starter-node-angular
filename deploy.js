9837fce32a5e63d283c1619a3c34cec1a1c6949f
Nelson Chaves <nelsonchavespro@gmail.com>
2020-03-29 21:29:49 -0700

    IP update

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.21.185.126',
+      host: '3.21.241.46',
       username: 'ubuntu',
       privateKey: 'sna-key.pem'
     })
