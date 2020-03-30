ff25090150f4be78ee1506ed74c54e3fce18d985
Nelson Chaves <nelsonchavespro@gmail.com>
2020-03-29 19:58:55 -0700

    Travis CI Deployment Automation reset

--- deploy.js
+++ deploy.js
@@ -22,8 +22,8 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../starter-node-angular',
-    '/home/ubuntu/starter-node-angular',
+    '../hackathon-starter',
+    '/home/ubuntu/hackathon-starter-temp',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +49,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf starter-node-angular-temp && mkdir starter-node-angular-temp', {
+    'rm -rf hackathon-starter-temp && mkdir hackathon-starter-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +65,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'mkdir starter-node-angular && cp -r starter-node-angular-temp/* starter-node-angular/ && rm -rf starter-node-angular-temp', {
+    'mkdir hackathon-starter && cp -r hackathon-starter-temp/* hackathon-starter/ && rm -rf hackathon-starter-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -73,7 +73,7 @@ function updateRemoteApp() {
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
   return ssh.execCommand(
-    'cd starter-node-angular && sudo service mongod start && pm2 start app.js', {
+    'cd hackathon-starter && sudo service mongod start && pm2 start app.js', {
       cwd: '/home/ubuntu'
   });
 }
@@ -95,7 +95,7 @@ function sshConnect() {
       return installPM2();
     })
     .then(function() {
-      console.log('Creating `starter-node-angular` folder.');
+      console.log('Creating `hackathon-starter-temp` folder.');
       return createRemoteTempFolder();
     })
     .then(function(result) {
